414c00b143a7669062461e5248841aaed6d2adab
root <root@jenks.rohnow.int>
2018-01-21 16:25:09 -0800

    adding build.xml file

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,13 +1,12 @@
 public class Rectangulator {
-	public static void main(String[] args) {
-		int length = Integer.parseInt(args[0]);
-		int width = Integer.parseInt(args[1]);
-	
-		Rectangle myRectangle = new Rectangle(length, width);
-	
-		String output = String.format("*** Your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+  public static void main(String[] args) {
+    int length = Integer.parseInt(args[0]);
+    int width = Integer.parseInt(args[1]);
 
-		system.out.println(output);
-	}
+    Rectangle myRectangle = new Rectangle(length, width);
 
+    String output = String.format("*** Your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+
+    System.out.println(output);
+  }
 }
